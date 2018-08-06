dac19251e8685706212631ca36f131771f603a10
Yonemura <nrtrstar@yahoo.co.jp>
2018-08-06 15:08:06 +0900

    UICanvas無効化コミット

--- webvr.js
+++ webvr.js
@@ -121,8 +121,8 @@
       return vrDisplay.requestPresent([{source: canvas}]).then(function () {
         // Start stereo rendering in Unity.
         console.log('Entered VR mode');
-        gameInstance.SendMessage('WebVRCameraSet', 'toggleVrState');
-        //gameInstance.SendMessage('WebVRCameraSet', 'OnStartVR');
+        //gameInstance.SendMessage('WebVRCameraSet', 'toggleVrState');
+        gameInstance.SendMessage('WebVRCameraSet', 'OnStartVR');
       }).catch(function (err) {
         console.error('Unable to enter VR mode:', err);
       });
